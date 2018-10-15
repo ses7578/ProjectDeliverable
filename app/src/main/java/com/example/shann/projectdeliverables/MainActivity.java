288b80fc13b39d9d7702fa5d637042a1d41c492c
Shannon Sloan <shannonsloan303@gmail.com>
2018-10-15 18:28:47 -0400

    Login

--- app/src/main/java/com/example/shann/projectdeliverables/MainActivity.java
+++ app/src/main/java/com/example/shann/projectdeliverables/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.shann.projectdeliverables;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private Button button;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                openActivity2();
+            }
+        });
+    }
+
+    public void openActivity2()
+    {
+        Intent intent = new Intent(this, AlarmLogin.class);
+        startActivity(intent);
     }
 }

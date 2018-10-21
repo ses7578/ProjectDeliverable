47ed71c0a1dc227f9a3d6fe5daa7114730015be6
Shannon Sloan <shannonsloan303@gmail.com>
2018-10-21 17:25:42 -0400

    hopefully working

--- app/src/main/java/com/example/shann/projectdeliverables/Alarm_Settings.java
+++ app/src/main/java/com/example/shann/projectdeliverables/Alarm_Settings.java
@@ -8,6 +8,6 @@ public class Alarm_Settings extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_alarm__settings);
+        setContentView(R.layout.activity_alarm_settings);
     }
 }
